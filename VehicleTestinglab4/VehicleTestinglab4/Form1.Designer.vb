2e1d2dfbcd61e5b57c0ccd31787125bda6f3c6ee
Harsimran sidhu <harsimransidhu770@gmail.com>
2017-02-03 21:12:40 -0500

    Quote generator for car Maintenance
    Program takes input from user, where user will select listed car they have based in that program will generate a text with all the detail pricing . User has to select what type of maintenance they would like to be done on the car. 
    Once the program has calculated the prices and tax . it will show a list of cost to user in multi line text box.

---
+++ VehicleTestinglab4/VehicleTestinglab4/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.vehicleList = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtReport = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RDBritishC = New System.Windows.Forms.RadioButton()
+        Me.RDAlberta = New System.Windows.Forms.RadioButton()
+        Me.RDOntario = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CHHybrid = New System.Windows.Forms.CheckBox()
+        Me.CHIncludeDD = New System.Windows.Forms.CheckBox()
+        Me.CHElectricalT = New System.Windows.Forms.CheckBox()
+        Me.CHExhuastST = New System.Windows.Forms.CheckBox()
+        Me.CKEngineTest = New System.Windows.Forms.CheckBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnGetQuote = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'vehicleList
+        '
+        Me.vehicleList.FormattingEnabled = True
+        Me.vehicleList.Items.AddRange(New Object() {"Tesla Model S", "Tesla Model X", "Tesla Model 3", "Ford Mustang", "Honda Civic", "Toyota Camry (Hybrid)", "Toyota Camry"})
+        Me.vehicleList.Location = New System.Drawing.Point(30, 64)
+        Me.vehicleList.Name = "vehicleList"
+        Me.vehicleList.Size = New System.Drawing.Size(206, 160)
+        Me.vehicleList.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Vehicle"
+        '
+        'txtReport
+        '
+        Me.txtReport.Location = New System.Drawing.Point(30, 272)
+        Me.txtReport.Multiline = True
+        Me.txtReport.Name = "txtReport"
+        Me.txtReport.ReadOnly = True
+        Me.txtReport.Size = New System.Drawing.Size(708, 198)
+        Me.txtReport.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RDBritishC)
+        Me.GroupBox1.Controls.Add(Me.RDAlberta)
+        Me.GroupBox1.Controls.Add(Me.RDOntario)
+        Me.GroupBox1.Location = New System.Drawing.Point(545, 68)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(166, 156)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Location"
+        '
+        'RDBritishC
+        '
+        Me.RDBritishC.AutoSize = True
+        Me.RDBritishC.Location = New System.Drawing.Point(18, 101)
+        Me.RDBritishC.Name = "RDBritishC"
+        Me.RDBritishC.Size = New System.Drawing.Size(99, 17)
+        Me.RDBritishC.TabIndex = 2
+        Me.RDBritishC.TabStop = True
+        Me.RDBritishC.Text = "British Columbia"
+        Me.RDBritishC.UseVisualStyleBackColor = True
+        '
+        'RDAlberta
+        '
+        Me.RDAlberta.AutoSize = True
+        Me.RDAlberta.Location = New System.Drawing.Point(18, 64)
+        Me.RDAlberta.Name = "RDAlberta"
+        Me.RDAlberta.Size = New System.Drawing.Size(58, 17)
+        Me.RDAlberta.TabIndex = 1
+        Me.RDAlberta.TabStop = True
+        Me.RDAlberta.Text = "Alberta"
+        Me.RDAlberta.UseVisualStyleBackColor = True
+        '
+        'RDOntario
+        '
+        Me.RDOntario.AutoSize = True
+        Me.RDOntario.Location = New System.Drawing.Point(18, 31)
+        Me.RDOntario.Name = "RDOntario"
+        Me.RDOntario.Size = New System.Drawing.Size(59, 17)
+        Me.RDOntario.TabIndex = 0
+        Me.RDOntario.TabStop = True
+        Me.RDOntario.Text = "Ontario"
+        Me.RDOntario.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CHHybrid)
+        Me.GroupBox2.Controls.Add(Me.CHIncludeDD)
+        Me.GroupBox2.Controls.Add(Me.CHElectricalT)
+        Me.GroupBox2.Controls.Add(Me.CHExhuastST)
+        Me.GroupBox2.Controls.Add(Me.CKEngineTest)
+        Me.GroupBox2.Location = New System.Drawing.Point(775, 49)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(244, 191)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Vehicle Tests"
+        '
+        'CHHybrid
+        '
+        Me.CHHybrid.AutoSize = True
+        Me.CHHybrid.Location = New System.Drawing.Point(24, 168)
+        Me.CHHybrid.Name = "CHHybrid"
+        Me.CHHybrid.Size = New System.Drawing.Size(149, 17)
+        Me.CHHybrid.TabIndex = 4
+        Me.CHHybrid.Text = "Battery Test (Only Hybrid!)"
+        Me.CHHybrid.UseVisualStyleBackColor = True
+        '
+        'CHIncludeDD
+        '
+        Me.CHIncludeDD.AutoSize = True
+        Me.CHIncludeDD.Location = New System.Drawing.Point(24, 135)
+        Me.CHIncludeDD.Name = "CHIncludeDD"
+        Me.CHIncludeDD.Size = New System.Drawing.Size(161, 17)
+        Me.CHIncludeDD.TabIndex = 3
+        Me.CHIncludeDD.Text = "Include Detailed Diagnostics"
+        Me.CHIncludeDD.UseVisualStyleBackColor = True
+        '
+        'CHElectricalT
+        '
+        Me.CHElectricalT.AutoSize = True
+        Me.CHElectricalT.Location = New System.Drawing.Point(24, 97)
+        Me.CHElectricalT.Name = "CHElectricalT"
+        Me.CHElectricalT.Size = New System.Drawing.Size(130, 17)
+        Me.CHElectricalT.TabIndex = 2
+        Me.CHElectricalT.Text = "Electrical System Test"
+        Me.CHElectricalT.UseVisualStyleBackColor = True
+        '
+        'CHExhuastST
+        '
+        Me.CHExhuastST.AutoSize = True
+        Me.CHExhuastST.Location = New System.Drawing.Point(24, 63)
+        Me.CHExhuastST.Name = "CHExhuastST"
+        Me.CHExhuastST.Size = New System.Drawing.Size(125, 17)
+        Me.CHExhuastST.TabIndex = 1
+        Me.CHExhuastST.Text = "Exhaust System Test"
+        Me.CHExhuastST.UseVisualStyleBackColor = True
+        '
+        'CKEngineTest
+        '
+        Me.CKEngineTest.AutoSize = True
+        Me.CKEngineTest.Location = New System.Drawing.Point(24, 30)
+        Me.CKEngineTest.Name = "CKEngineTest"
+        Me.CKEngineTest.Size = New System.Drawing.Size(83, 17)
+        Me.CKEngineTest.TabIndex = 0
+        Me.CKEngineTest.Text = "Engine Test"
+        Me.CKEngineTest.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox1.Location = New System.Drawing.Point(242, 24)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(297, 216)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'btnGetQuote
+        '
+        Me.btnGetQuote.Location = New System.Drawing.Point(764, 272)
+        Me.btnGetQuote.Name = "btnGetQuote"
+        Me.btnGetQuote.Size = New System.Drawing.Size(271, 197)
+        Me.btnGetQuote.TabIndex = 6
+        Me.btnGetQuote.Text = "Get Quote"
+        Me.btnGetQuote.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(878, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(169, 39)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1050, 507)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGetQuote)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.vehicleList)
+        Me.Controls.Add(Me.txtReport)
+        Me.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Vehicle Testing Program"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents vehicleList As ListBox
+    Friend WithEvents Label1 As Label
+    Private WithEvents txtReport As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RDBritishC As RadioButton
+    Friend WithEvents RDAlberta As RadioButton
+    Friend WithEvents RDOntario As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents CHIncludeDD As CheckBox
+    Friend WithEvents CHElectricalT As CheckBox
+    Friend WithEvents CHExhuastST As CheckBox
+    Friend WithEvents CKEngineTest As CheckBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnGetQuote As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents CHHybrid As CheckBox
+End Class
---
+++ VehicleTestinglab4/VehicleTestinglab4/Form1.vb
@@ -0,0 +1,355 @@
+﻿Public Class Form1
+    Dim selection As String 'Variable for car selection
+
+    Dim TAX As Double ' Tax variable
+
+
+    Dim EngineTest As Double 'Enginetest variable
+    Dim ExhuastSTest As Double 'exhuast system test variable
+    Dim ElectricalTest As Double 'electrical test variable
+    Dim BatteryTestOnlyHybrid As Double 'battery test for hybrid variable
+    Dim IncludeDD As Double 'diagonostic test variabe
+
+    Dim IncludeDDiagonostic As Boolean 'boolean so if condition could determine user has selected the diagnostic test
+
+    Dim HourlyCost As Integer 'hourly cost variable
+
+
+    Dim Subtotal As Double 'subtotal varible
+    Dim Total As Double ' Total variable
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load ' form load up
+        CHHybrid.Visible = False 'make the hybrid option visible false 
+
+        TAX = 0 ' make TAX variable zero
+
+    End Sub
+    Private Sub vehicleList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles vehicleList.SelectedIndexChanged
+
+        EngineTest = 3.0 'engine test will take 3 hours
+        ElectricalTest = 2 'electrical test will take 2 hours
+        ExhuastSTest = 1 'exhuast test 1 hour
+        BatteryTestOnlyHybrid = 2 'Battery test only for hybrid will take 2 hours
+
+        selection = vehicleList.Text 'put the selection or text when user picks from listbox in selection variable
+
+        Select Case selection ' select case condition on selection variable
+            Case "Tesla Model S" 'if this condition matches
+                CKEngineTest.Text = "Battery Test" 'make engine test label show battery test for electrical vehicle
+                CHHybrid.Visible = False 'make hybrid only varible visible false
+                CHHybrid.Checked = False 'and uncheck the battery test for hybrid check box
+                CHExhuastST.Text = "Auto Drive System Test" 'make exhuast test label to auto drive test for smart cars or TESLA
+                PictureBox1.Image = My.Resources.CW7whm 'show logo of Tesla on picture box
+
+
+                HourlyCost = 80 ' assign hourly cost 
+            Case "Tesla Model X"
+                CKEngineTest.Text = "Battery Test" 'make engine test label show battery test for electrical vehicle
+                CHExhuastST.Text = "Auto Drive System Test" 'make exhuast test label to auto drive test for smart cars or TESLA
+                CHHybrid.Visible = False 'make hybrid only varible visible false
+                CHHybrid.Checked = False
+                PictureBox1.Image = My.Resources.CW7whm 'show logo of Tesla on picture box
+
+                HourlyCost = 150' assign hourly cost 
+            Case "Tesla Model 3"
+                CKEngineTest.Text = "Battery Test" 'make engine test label show battery test for electrical vehicle
+                CHExhuastST.Text = "Auto Drive System Test" 'make exhuast test label to auto drive test for smart cars or TESLA
+                CHHybrid.Visible = False 'make hybrid only varible visible false
+                CHHybrid.Checked = False
+                PictureBox1.Image = My.Resources.CW7whm 'show logo of Tesla on picture box
+
+
+                HourlyCost = 100' assign hourly cost 
+            Case "Ford Mustang"
+                CKEngineTest.Text = "Engine Test" 'only show engine test label to cars with engine 
+                CHExhuastST.Text = "Exhaust System Test" 'show exhaust test label for car with exhuast available 
+                CHHybrid.Visible = False 'make hybrid only varible visible false
+                CHHybrid.Checked = False
+                PictureBox1.Image = My.Resources.images 'show logo of Mustang on picture box
+
+
+                HourlyCost = 80' assign hourly cost 
+            Case "Honda Civic"
+                CKEngineTest.Text = "Engine Test" 'only show engine test label To cars With engine 
+                CHHybrid.Visible = False 'make hybrid only varible visible false
+                CHHybrid.Checked = False
+                CHExhuastST.Text = "Exhaust System Test" 'show exhaust test label for car with exhuast available 
+                PictureBox1.Image = My.Resources.HondaLogo 'show logo of Honda on picture box
+
+
+                HourlyCost = 90' assign hourly cost 
+            Case "Toyota Camry (Hybrid)"
+                CKEngineTest.Text = " Engine Test" 'only show engine test label To cars With engine 
+                CHExhuastST.Text = "Exhaust System Test" 'show exhaust test label for car with exhuast available 
+                CHHybrid.Visible = True 'make hybrid only varible visible true for hybrid cars
+                CHHybrid.Checked = False
+                PictureBox1.Image = My.Resources.download 'show logo of Toyota on picture box
+
+
+                HourlyCost = 200' assign hourly cost 
+            Case "Toyota Camry"
+                CKEngineTest.Text = " Engine Test" 'only show engine test label To cars With engine 
+                CHExhuastST.Text = "Exhaust System Test" 'show exhaust test label for car with exhuast available 
+                CHHybrid.Visible = False 'make hybrid only varible visible false
+                CHHybrid.Checked = False
+                PictureBox1.Image = My.Resources.download 'show logo of Toyota on picture box
+
+
+                HourlyCost = 200 ' assign hourly cost 
+        End Select
+    End Sub
+
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close() ' close the form once button is clicked
+    End Sub
+
+    Private Sub btnGetQuote_Click(sender As Object, e As EventArgs) Handles btnGetQuote.Click
+        IncludeDDiagonostic = False ' make sure includeDDiagonostic bollean is false before checking for any check boxes are checked
+        Subtotal = 0 'make subtotal 0 every time get qoute button is pressed . so it doesnt addup with previous value
+        Select Case selection ' Do a Case condition on selection variable again to check which vehicle is selected
+            Case "Tesla Model S"
+                If CKEngineTest.Checked = True Then ' if user select battery test option for tesla then..
+                    Subtotal = Subtotal + (EngineTest * HourlyCost) 'first add subtotal with hourly pay
+
+                End If
+                If CHElectricalT.Checked = True Then 'if user select option for electrical system test...
+                    Subtotal = Subtotal + (ElectricalTest * HourlyCost) 'add up the cost by adding prices to subtotal
+
+                End If
+                If CHExhuastST.Checked = True Then ' and so on...
+                    Subtotal = Subtotal + (ExhuastSTest * HourlyCost) ' calculations forexhuast system test.
+
+                End If
+
+                If CHIncludeDD.Checked = True Then 'if user checked the include diagnostic test then
+                    IncludeDDiagonostic = True 'make this value true for future
+                    Subtotal = Subtotal * 2 'double up the subtotal
+                    IncludeDD = 50 ' add extra 50 on the price
+
+                End If
+            Case "Tesla Model X" ' and so on..
+                If CKEngineTest.Checked = True Then
+                    Subtotal = Subtotal + (EngineTest * HourlyCost)
+
+                End If
+                If CHElectricalT.Checked = True Then
+                    Subtotal = Subtotal + (ElectricalTest * HourlyCost)
+
+                End If
+                If CHExhuastST.Checked = True Then
+                    Subtotal = Subtotal + (ExhuastSTest * HourlyCost)
+
+                End If
+
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    Subtotal = Subtotal * 2
+                    IncludeDD = 50
+
+                End If
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    IncludeDD = 50
+                End If
+            Case "Tesla Model 3"
+                If CKEngineTest.Checked = True Then
+                    Subtotal = Subtotal + (EngineTest * HourlyCost)
+
+                End If
+                If CHElectricalT.Checked = True Then
+                    Subtotal = Subtotal + (ElectricalTest * HourlyCost)
+
+                End If
+                If CHExhuastST.Checked = True Then
+                    Subtotal = Subtotal + (ExhuastSTest * HourlyCost)
+
+                End If
+
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    Subtotal = Subtotal * 2
+                    IncludeDD = 50
+
+                End If
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    IncludeDD = 50
+                End If
+            Case "Ford Mustang"
+                If CKEngineTest.Checked = True Then
+                    Subtotal = Subtotal + (EngineTest * HourlyCost)
+
+                End If
+                If CHElectricalT.Checked = True Then
+                    Subtotal = Subtotal + (ElectricalTest * HourlyCost)
+
+                End If
+                If CHExhuastST.Checked = True Then
+                    Subtotal = Subtotal + (ExhuastSTest * HourlyCost)
+
+                End If
+
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    Subtotal = Subtotal * 2
+                    IncludeDD = 50
+
+                End If
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    IncludeDD = 50
+                End If
+            Case "Honda Civic"
+                If CKEngineTest.Checked = True Then
+                    Subtotal = Subtotal + (EngineTest * HourlyCost)
+
+                End If
+                If CHElectricalT.Checked = True Then
+                    Subtotal = Subtotal + (ElectricalTest * HourlyCost)
+
+                End If
+                If CHExhuastST.Checked = True Then
+                    Subtotal = Subtotal + (ExhuastSTest * HourlyCost)
+
+                End If
+
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    Subtotal = Subtotal * 2
+                    IncludeDD = 50
+
+                End If
+            Case "Toyota Camry (Hybrid)"
+                If CKEngineTest.Checked = True Then
+                    Subtotal = Subtotal + (EngineTest * HourlyCost)
+
+                End If
+                If CHElectricalT.Checked = True Then
+                    Subtotal = Subtotal + (ElectricalTest * HourlyCost)
+
+                End If
+                If CHExhuastST.Checked = True Then
+                    Subtotal = Subtotal + (ExhuastSTest * HourlyCost)
+
+                End If
+                If CHHybrid.Checked = True Then ' if battery test for hybrid option is checked then...
+                    Subtotal = Subtotal + (BatteryTestOnlyHybrid * HourlyCost) ' do the same calculation and add up the price
+                End If
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    Subtotal = Subtotal * 2
+                    IncludeDD = 50
+
+                End If
+            Case "Toyota Camry"
+                If CKEngineTest.Checked = True Then
+                    Subtotal = Subtotal + (EngineTest * HourlyCost)
+
+                End If
+                If CHElectricalT.Checked = True Then
+                    Subtotal = Subtotal + (ElectricalTest * HourlyCost)
+
+                End If
+                If CHExhuastST.Checked = True Then
+                    Subtotal = Subtotal + (ExhuastSTest * HourlyCost)
+
+                End If
+
+                If CHIncludeDD.Checked = True Then
+                    IncludeDDiagonostic = True
+                    Subtotal = Subtotal * 2
+                    IncludeDD = 50
+
+                End If
+
+        End Select
+
+
+
+        If RDOntario.Checked = True Then ' if check box for Ontario tax is selected then..
+            TAX = 0.13 'make TAX variable 13 % or 0.13 in this case 
+        End If
+        If RDAlberta.Checked = True Then ' same thing with Alberta..
+            TAX = 0.05 'TAX = 5 % or 0.05
+        End If
+        If RDBritishC.Checked = True Then 'Same thing with BC..
+            TAX = 0.07
+        End If
+        If RDAlberta.Checked = False AndAlso RDBritishC.Checked = False AndAlso RDOntario.Checked = False Then ' if no option for tax is selected warn user with a msg box
+            Call MsgBox("ERROR", 0, "Please Select TAX option") ' show msg box with Title Error. and wearning showing Please choose a region
+        End If
+
+
+        txtReport.Text = " Vehicle Testing Quote" & vbCrLf 'Print Title first in the text box
+        txtReport.Text += " " & vbCrLf ' Space
+        txtReport.Text += "Vehicle: " & selection & vbCrLf 'Show which vehicle is selected
+        txtReport.Text += " " & vbCrLf 'space
+        If CKEngineTest.Checked = True Then ' if engine test check box is checked then
+            If selection = "Ford Mustang" Or selection = "Toyota Camry (Hybrid)" Or selection = "Toyota Camry" Or selection = "Honda Civic" Then ' if any of cars with engine is selected
+                txtReport.Text += "Engine Test:" & FormatCurrency(EngineTest * HourlyCost, 2) ' make sure print engine test in text box
+                If IncludeDDiagonostic = False Then ' and if Diagonostic test is not selected
+                    txtReport.Text += vbCrLf 'skip the line
+                End If
+            Else
+                txtReport.Text += "Battery Test:" & FormatCurrency(EngineTest * HourlyCost, 2) ' else print the following for Electrical cars
+                If IncludeDDiagonostic = False Then 'and if Diagonostic test is not selected
+                    txtReport.Text += vbCrLf ' skip this line
+                End If
+            End If
+            If IncludeDDiagonostic = True Then ' if it is true then
+                txtReport.Text += Space(25) & "With Detailed Diagnostics: " & FormatCurrency(EngineTest * HourlyCost, 2) & vbCrLf 'space it out 25 times and display with how many cost will it take for single test
+            End If
+        End If
+
+        If CHExhuastST.Checked = True Then
+            If selection = "Ford Mustang" Or selection = "Toyota Camry (Hybrid)" Or selection = "Toyota Camry" Or selection = "Honda Civic" Then
+                txtReport.Text += "Exhaust System Tests:" & FormatCurrency(ExhuastSTest * HourlyCost, 2)
+                If IncludeDDiagonostic = False Then
+                    txtReport.Text += vbCrLf
+                End If
+            Else
+                txtReport.Text += "AutoDrive System Test:" & FormatCurrency(ExhuastSTest * HourlyCost, 2)
+                If IncludeDDiagonostic = False Then
+                    txtReport.Text += vbCrLf
+                End If
+            End If
+            If IncludeDDiagonostic = True Then
+                txtReport.Text += Space(10) & "With Detailed Diagnostics: " & FormatCurrency(ExhuastSTest * HourlyCost, 2) & vbCrLf
+            End If
+        End If
+
+
+        If CHExhuastST.Checked = True Then
+            txtReport.Text += "Electrical System Tests:" & FormatCurrency(ElectricalTest * HourlyCost, 2)
+            If IncludeDDiagonostic = False Then
+                txtReport.Text += vbCrLf
+            Else
+                txtReport.Text += Space(9) & "With Detailed Diagnostics: " & FormatCurrency(ElectricalTest * HourlyCost, 2) & vbCrLf
+            End If
+        End If
+
+        If CHHybrid.Checked = True Then
+            txtReport.Text += "Battery Tests:" & FormatCurrency(BatteryTestOnlyHybrid * HourlyCost, 2)
+            If IncludeDDiagonostic = False Then
+                txtReport.Text += vbCrLf
+            End If
+            If IncludeDDiagonostic = True Then
+                txtReport.Text += Space(25) & "With Detailed Diagnostics: " & FormatCurrency(BatteryTestOnlyHybrid * HourlyCost, 2) & vbCrLf
+            End If
+        End If
+
+        If CHIncludeDD.Checked = True Then
+            txtReport.Text += Space(70) & "Diagnostics Set Up Fee:" & FormatCurrency(IncludeDD) & vbCrLf
+            If IncludeDDiagonostic = False Then
+                txtReport.Text += vbCrLf
+            End If
+        End If
+        txtReport.Text += vbCrLf
+        txtReport.Text += "Subtotal:" & Space(16) & FormatCurrency(Subtotal, 2) & vbCrLf 'show subtotal without tax added
+        txtReport.Text += FormatPercent(TAX) & " Tax:" & Space(10) & FormatCurrency(Subtotal * TAX, 2) & vbCrLf ' show which option for tax was choosen and how much you will be taxed
+        Total = Subtotal + (Subtotal * TAX) ' do the calculation with tax added to subtotal and saved into total variable
+        txtReport.Text += "Final Total:" & Space(13) & FormatCurrency(Total, 2) & vbCrLf 'show user with total
+    End Sub
+End Class
